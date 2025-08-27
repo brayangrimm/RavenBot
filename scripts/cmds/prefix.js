78d8c3ef1a130050e8cf2ea0236aa289f999d461
brayangrimm <dgbrayan896@gmail.com>
2025-08-27 05:30:26 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
                         confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
                         successGlobal: "Changed prefix of system bot to: %1",
                         successThisThread: "Changed prefix in your box chat to: %1",
-                        myPrefix: "/)    /)───────◆\n(｡•ㅅ•｡) ❥BRAYAN ㋛ᗷOT\n╭∪─∪───────◆\n╰🙂 C'est mon préfixe, tu vois pas ? ➤ %1"
+                        myPrefix: "/)    /)───────◆\n(｡•ㅅ•｡) ❥ TAILS ㋛ᗷOT\n╭∪─∪───────◆\n╰😇 voilà mon prefix ➤ %1"
                 }
         },
 
